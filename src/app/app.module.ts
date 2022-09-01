93b17fda78492676457c7e07b8d5b43d317aeeed
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-08-31 23:34:53 -0300

    Componentes y clase Usuario agregadas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    ErrorComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
